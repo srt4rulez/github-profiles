67060a4e2b14ef9085d4c12893cc24b5080ad1b9
Jake Dennison <srt4rulez@gmail.com>
2022-02-19 22:47:30 -0500

    feat(graphql): add apollo client back to index with new netlify function

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'src/App';
-// import {
-//     ApolloClient,
-//     InMemoryCache,
-//     ApolloProvider,
-// } from '@apollo/client';
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+} from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from 'src/theme';
 import '@fontsource/rubik/400.css';
 import '@fontsource/rubik/700.css';
 
-// const client = new ApolloClient({
-//     uri: typeof window !== 'undefined' ? `${window.location.origin}/api/graphql` : undefined,
-//     cache: new InMemoryCache(),
-// });
+const client = new ApolloClient({
+    uri: `${window.location.origin}/.netlify/functions/graphql`,
+    cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
     <React.StrictMode>
-        {/* <ApolloProvider */}
-        {/*     client={client} */}
-        {/* > */}
-
-        <ThemeProvider
-            theme={theme}
+        <ApolloProvider
+            client={client}
         >
 
-            <ChakraProvider
+            <ThemeProvider
                 theme={theme}
             >
 
-                <App />
+                <ChakraProvider
+                    theme={theme}
+                >
 
-            </ChakraProvider>
+                    <App />
 
-        </ThemeProvider>
+                </ChakraProvider>
 
-        {/* </ApolloProvider> */}
+            </ThemeProvider>
+
+        </ApolloProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
