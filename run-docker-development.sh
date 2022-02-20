0e688c1bbe6988c11a660602689e0b0a2e4197e2
Jake Dennison <srt4rulez@gmail.com>
2022-02-20 15:24:51 -0500

    docs: update readme with netlify instructions, update docker container script to include netlify

--- run-docker-development.sh
+++ run-docker-development.sh
@@ -2,6 +2,11 @@
 
 docker_image="node:16"
 repo_name="github-profiles"
+user="node"
+
+# Create netlify's config directory on the host.
+# This will be mounted from host to container to save netlify config when working in docker.
+mkdir -p ~/.config/netlify/
 
 # https://docs.docker.com/engine/reference/commandline/run/
 args=(
@@ -18,16 +23,22 @@ args=(
     --rm
 
     # Publish a container's port(s) to the host
-    --publish 3000:3000 # nextjs's default server port.
+    --publish 3000:3000 # create react app's default server port.
+
+    --publish 8888:8888 # netlify's dev server port.
 
     # Bind mount a volume from the <host>:<container>
     --volume "$(pwd):/$repo_name" # mount the project directory to a directory of the repo name.
+
+    # Bind mount host's netlify config to docker container to prevent having to
+    # setup netlify everytime we run docker.
+    --volume "$HOME/.config/netlify:/home/$user/.config/netlify"
 
     # Working directory inside the container (the starting directory when the container runs)
     --workdir /"$repo_name"
 
     # Username or UID (format: <name|uid>[:<group|gid>])
-    --user node
+    --user "$user"
 )
 
 sudo docker run "${args[@]}" "$docker_image" /bin/bash
