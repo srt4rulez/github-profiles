52cd17db5f5e72bdbe2a320db1f3ddc98f680c2e
Jake Dennison <srt4rulez@gmail.com>
2022-02-20 16:22:19 -0500

    update autocomplete with font awesome icons, add OptionInterface

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,20 @@ import {
 import {
     Autocomplete,
 } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faChevronDown,
+    faXmark,
+} from '@fortawesome/free-solid-svg-icons';
+import { css } from '@emotion/css';
+
+interface OptionInterface {
+    id: string;
+    login: string;
+    name: string;
+    isViewer: boolean;
+    avatarUrl: string;
+}
 
 const App = (): JSX.Element | null => {
 
@@ -28,7 +42,7 @@ const App = (): JSX.Element | null => {
         >
 
             <Box
-                as="section"
+                as="header"
                 marginTop="32"
             >
 
@@ -57,125 +71,157 @@ const App = (): JSX.Element | null => {
 
             </Box>
 
-            <Autocomplete
-                id="github-username"
-                onInputChange={(event, value): void => setInputValue(value)}
-                inputValue={inputValue}
-                options={[
-                    {
-                        'id': 'MDQ6VXNlcjY2MTAyMA==',
-                        'login': 'srt4',
-                        'name': 'Spencer Thomas',
-                        'isViewer': false,
-                        'avatarUrl': 'https://avatars.githubusercontent.com/u/661020?v=4'
-                    },
-                    {
-                        'id': 'MDQ6VXNlcjQ3MTg1NDI=',
-                        'login': 'srt4rulez',
-                        'name': 'Jake D',
-                        'isViewer': true,
-                        'avatarUrl': 'https://avatars.githubusercontent.com/u/4718542?u=b6f6c9dc2164afb3d138a7acda8bcf17f7d3ad5f&v=4'
-                    },
-                    {
-                        'id': 'MDQ6VXNlcjQwMDM5ODE0',
-                        'login': 'srt499',
-                        'name': 'Brian Porter',
-                        'isViewer': false,
-                        'avatarUrl': 'https://avatars.githubusercontent.com/u/40039814?v=4'
-                    },
-                ]}
-                open={isOpen}
-                onOpen={onOpen}
-                onClose={onClose}
-                getOptionLabel={(option): string => option.login}
-                PaperComponent={(paperProps): JSX.Element => {
-                    return (
-                        <Box
-                            position="relative"
-                            top="2px"
-                            boxShadow="1"
-                            rounded="md"
-                            borderWidth="1px"
-                            borderColor="gray.200"
-                        >
-                            {paperProps.children}
-                        </Box>
-                    );
-                }}
-                renderOption={(optionProps, option): JSX.Element => {
-                    return (
-                        // @ts-expect-error: chakra and mui html types are out of sync
-                        <Box
-                            as="li"
-                            {...optionProps}
-                        >
+            <Box
+                as="section"
+                maxWidth="400px"
+                marginX="auto"
+                marginTop="5"
+            >
 
-                            <Avatar
-                                name={option.name}
-                                src={option.avatarUrl}
-                                size="sm"
-                                marginRight="3"
-                                loading="lazy"
-                            />
-
-                            <Box>
-                                <Text
-                                    color="gray.800"
-                                >
-                                    {option.login}
-                                </Text>
-
-                                <Text
-                                    color="gray.500"
-                                    fontSize="sm"
-                                >
-                                    {option.name}
-                                </Text>
-
+                <Autocomplete
+                    id="github-username"
+                    onInputChange={(event, value): void => setInputValue(value)}
+                    inputValue={inputValue}
+                    classes={{
+                        clearIndicator: css({
+                            marginRight: '5px !important',
+                            backgroundColor: 'transparent !important',
+                        }),
+                        popupIndicator: css({
+                            backgroundColor: 'transparent !important',
+                        }),
+                    }}
+                    options={[
+                        {
+                            'id': 'MDQ6VXNlcjY2MTAyMA==',
+                            'login': 'srt4',
+                            'name': 'Spencer Thomas',
+                            'isViewer': false,
+                            'avatarUrl': 'https://avatars.githubusercontent.com/u/661020?v=4'
+                        },
+                        {
+                            'id': 'MDQ6VXNlcjQ3MTg1NDI=',
+                            'login': 'srt4rulez',
+                            'name': 'Jake D',
+                            'isViewer': true,
+                            'avatarUrl': 'https://avatars.githubusercontent.com/u/4718542?u=b6f6c9dc2164afb3d138a7acda8bcf17f7d3ad5f&v=4'
+                        },
+                        {
+                            'id': 'MDQ6VXNlcjQwMDM5ODE0',
+                            'login': 'srt499',
+                            'name': 'Brian Porter',
+                            'isViewer': false,
+                            'avatarUrl': 'https://avatars.githubusercontent.com/u/40039814?v=4'
+                        },
+                    ]}
+                    open={isOpen}
+                    onOpen={onOpen}
+                    onClose={onClose}
+                    clearIcon={(
+                        <FontAwesomeIcon
+                            icon={faXmark}
+                            size="sm"
+                        />
+                    )}
+                    popupIcon={(
+                        <FontAwesomeIcon
+                            icon={faChevronDown}
+                            size="sm"
+                        />
+                    )}
+                    getOptionLabel={(option: OptionInterface): string => option.login}
+                    PaperComponent={(paperProps): JSX.Element => {
+                        return (
+                            <Box
+                                position="relative"
+                                top="2px"
+                                boxShadow="1"
+                                rounded="md"
+                                borderWidth="1px"
+                                borderColor="gray.200"
+                            >
+                                {paperProps.children}
                             </Box>
-
-                        </Box>
-                    );
-                }}
-                renderInput={(params): JSX.Element => {
-                    return (
-                        <FormControl
-                            id={params.id}
-                            label="Combo box"
-                            isDisabled={params.disabled}
-                            ref={params.InputProps.ref}
-                        >
-
-                            <FormLabel
-                                {...params.InputLabelProps}
-                            >
-                                Search by Username
-                            </FormLabel>
-
-                            <InputGroup
-                                size="lg"
+                        );
+                    }}
+                    renderOption={(optionProps, option: OptionInterface): JSX.Element => {
+                        return (
+                            // @ts-expect-error: chakra and mui html types are out of sync
+                            <Box
+                                as="li"
+                                {...optionProps}
                             >
 
-                                <Input
-                                    id={params.id}
-                                    {...params.inputProps}
-                                    size="lg"
-                                    paddingRight="75px"
+                                <Avatar
+                                    name={option.name}
+                                    src={option.avatarUrl}
+                                    size="sm"
+                                    marginRight="3"
+                                    loading="lazy"
                                 />
 
-                                <InputRightElement
-                                    width="60px"
-                                    right="4"
+                                <Box>
+
+                                    <Text
+                                        color="gray.800"
+                                    >
+                                        {option.login}
+                                    </Text>
+
+                                    <Text
+                                        color="gray.500"
+                                        fontSize="sm"
+                                    >
+                                        {option.name}
+                                    </Text>
+
+                                </Box>
+
+                            </Box>
+                        );
+                    }}
+                    renderInput={(params): JSX.Element => {
+                        return (
+                            <FormControl
+                                id={params.id}
+                                label="Search By Username"
+                                isDisabled={params.disabled}
+                                ref={params.InputProps.ref}
+                            >
+
+                                <FormLabel
+                                    {...params.InputLabelProps}
                                 >
-                                    {params.InputProps.endAdornment}
-                                </InputRightElement>
+                                    Search by Username
+                                </FormLabel>
 
-                            </InputGroup>
+                                <InputGroup
+                                    size="lg"
+                                >
 
-                        </FormControl>
-                    );
-                }}
-            />
+                                    <Input
+                                        id={params.id}
+                                        {...params.inputProps}
+                                        size="lg"
+                                        paddingRight="75px"
+                                        placeholder="Search by Username"
+                                    />
+
+                                    <InputRightElement
+                                        width="60px"
+                                        right="4"
+                                    >
+                                        {params.InputProps.endAdornment}
+                                    </InputRightElement>
+
+                                </InputGroup>
+
+                            </FormControl>
+                        );
+                    }}
+                />
+
+            </Box>
 
         </Container>
 
